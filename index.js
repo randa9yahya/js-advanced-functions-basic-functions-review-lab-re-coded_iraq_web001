a23c6614b933314b2b1df0ecba1405af82395a11
randa yahya <randaalrayan96@gmial.com>
2020-07-04 20:34:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 function saturdayFun( ) {
   return "This Saturday, I want to ....!"
 }
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
-}
+// function saturdayFun(activity="shoping") {
+//   return `This Saturday, I want to ${activity}!`
+// }
 
-let mondayWork = function(activity="go to the office") {
+let mondayWork = function(activity="go to the zoo") {
   return `This Monday, I will ${activity}.`
 }
 
@@ -14,4 +14,28 @@ let wrapAdjective = function(style="*") {
   return function(adjective="special") {
     return `You are ${style}${adjective}${style}!`
   }
+}
+const Calculator = {
+  add: function(a,b) {
+    return a + b;
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b;
+  }
+}
+
+let actionApplyer = function(start, ray) {
+  let a = start
+
+  for (let i = 0; i < ray.length; i++ ){
+    a = ray[i](a)
+  }
+
+  return a
 }
