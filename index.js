42f3601c408672e7e6ef77bc60f757516536f2b0
randa yahya <randaalrayan96@gmial.com>
2020-07-04 20:32:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 function saturdayFun( ) {
   return "This Saturday, I want to ....!"
 }
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(style="*") {
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
